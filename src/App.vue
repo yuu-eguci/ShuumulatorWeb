8626184e6d240b2af36b50112b7f983af1c3c7c5
yuu-eguci <yuu.eguci@gmail.com>
2021-03-28 18:33:52 +0900

    🔥 Needless prop

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="m-3">
-    <router-link :to="'/signin'">SignIn</router-link>
+    <router-link to="/signin">SignIn</router-link>
     <b-form-select v-model="$i18n.locale" :options="['en', 'ja']" size="sm" />
     <router-view />
   </div>
