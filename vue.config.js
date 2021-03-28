8404182756de38748676cf01bd9d2da2b07b619e
yuu-eguci <yuu.eguci@gmail.com>
2021-03-28 18:07:21 +0900

    🔧 vue.config.js publicPath

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,9 @@ module.exports = {
       localeDir: 'locales',
       enableInSFC: false
     }
-  }
+  },
+  // サブドメイン時のためのパラメータです。
+  // ./ にすると assets は解決する。だけど spa が解決しなかった。
+  // TODO: カスタムドメインでの運用が始まったら消すこと。
+  publicPath: '/ShuumulatorWeb/'
 }
