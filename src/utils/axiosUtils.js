2f84acef1f2b46a848fdadb6c81cdf8696fbcf09
yuu-eguci <yuu.eguci@gmail.com>
2021-04-14 13:35:04 +0900

    🔧 .env.local_ .env.production

--- src/utils/axiosUtils.js
+++ src/utils/axiosUtils.js
@@ -5,7 +5,7 @@ export default {
   createAxiosInstance: function (jwtToken) {
 
     return axios.create({
-      baseURL: 'http://localhost:8000',  // process.env.VUE_APP_API_BASE_URL,
+      baseURL: process.env.VUE_APP_API_BASE_URL,
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `JWT ${jwtToken}`,
