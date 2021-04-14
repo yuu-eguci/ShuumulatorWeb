e38e3cb27359c2f6b0390d0f8292c714e7abc77c
yuu-eguci <yuu.eguci@gmail.com>
2021-04-14 22:57:59 +0900

    💄 言語変更セレクトボックスの位置を調整

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,21 @@
     id="app"
     class="m-3"
   >
-    <b-form-select
-      v-model="$i18n.locale"
-      :options="['en', 'ja']"
-      size="sm"
-    />
     <router-view />
+    <b-row class="mt-5">
+      <b-col md="4" />
+      <b-col md="4">
+        <b-form-select
+          v-model="$i18n.locale"
+          :options="[
+            { value: 'en', text: 'English' },
+            { value: 'ja', text: '日本語で OK' },
+          ]"
+          size="sm"
+        />
+      </b-col>
+      <b-col md="4" />
+    </b-row>
   </div>
 </template>
 
